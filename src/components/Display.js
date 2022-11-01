b31c2a8e8a41481983d79480a0025c0f6fde1709
Marcelearn <jaworskimarcel@gmail.com>
2022-11-01 15:48:37 +0100

    build fixed

--- src/components/Display.js
+++ src/components/Display.js
@@ -4,9 +4,7 @@ function Display ({previous, current, oper}) {
     return(
         <div className='Display'>
             <div className='current'>{current} {oper}</div>
-            
-            <div className='previous'>{previous}</div>
-                
+            <div className='previous'>{previous}</div>        
         </div>
     )
 }
